979a6d90b249d1b4ae354f58d5286f39150fc0d5
Arvin Josol <86941482+Arvton@users.noreply.github.com>
2024-10-28 17:11:44 +0000

    add s3 and dynamodb

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,9 @@
-terraform {}
+terraform {
+  required_version = ">= 1.9.5"
+  backend "s3" {
+    bucket         = "arv-gitops-tf-backend"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "GitopsTerraformLocks"
+  }
+}
